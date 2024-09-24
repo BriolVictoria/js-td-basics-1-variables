2cdfec8e2cef2791294b5434a9b0075d80e52b04
BriolVictoria <victoria.briol@student.hepl.be>
2024-09-24 12:17:08 +0200

    test

--- starter/script00.js
+++ starter/script00.js
@@ -1,1 +1,20 @@
 console.log("Hello, World !");
+
+let myVar = "bonjour";
+let myNumber = 1 + 1;
+let myBool = false;
+let myBool2 = true;
+
+myVar = 'toto';
+myNumber = "2";
+
+const myConst = "bonjour";
+
+alert("bonjourrrrrr")
+
+const myAge = prompt("quel est ton age")
+
+console.log(myVar);
+
+
+
